7e5e7c0d70eae9edad75603e3224024e4d82a606
Mimak <mim4k1@gmail.com>
2020-07-10 20:09:01 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿using System.Drawing;
+
+namespace PW_zadanie_4_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.warningLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 389);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Visible = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(26, 37);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(575, 333);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(648, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // warningLabel
+            // 
+            this.warningLabel.AutoSize = true;
+            this.warningLabel.ForeColor = System.Drawing.Color.Red;
+            this.warningLabel.Location = new System.Drawing.Point(607, 222);
+            this.warningLabel.Name = "warningLabel";
+            this.warningLabel.Size = new System.Drawing.Size(35, 13);
+            this.warningLabel.TabIndex = 4;
+            this.warningLabel.Text = "label1";
+            this.warningLabel.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(621, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Elementy (n):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(619, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Permutacje (!n):";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.AsciiOnly = true;
+            this.maskedTextBox1.Location = new System.Drawing.Point(706, 157);
+            this.maskedTextBox1.Mask = "000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(25, 20);
+            this.maskedTextBox1.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(706, 185);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(82, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.progressLabel.AutoSize = true;
+            this.progressLabel.Location = new System.Drawing.Point(23, 428);
+            this.progressLabel.Name = "label3";
+            this.progressLabel.Size = new System.Drawing.Size(0, 13);
+            this.progressLabel.TabIndex = 9;
+            this.progressLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(800, 462);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.warningLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Generator permutacji";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label warningLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label progressLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PW_zadanie_4_1
+{
+    public partial class Form1 : Form
+    {
+        private static bool bGoing = false;
+        public static bool bInvalidUserInput = false;
+        public static bool bPrintOutput;
+        public static string lastWarning = "";
+        private static int lp = 0;
+        private static int refreshTime = 200;
+        private static Thread monitorThread, computationThread;
+        private delegate void SafeCallDelegate(int n);
+        private delegate void SafeCallDelegate2(List<byte>list, int lp);
+        private delegate void SafeCallDelegate3();
+        private delegate void SafeCallDelegate4(int curr, int NbPerms);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private byte GetNbElements()
+        {
+            string text = maskedTextBox1.Text;
+            try
+            {
+                if (Convert.ToInt32(text) > 12 || text.Length > 3 || text.Length == 0)
+                {
+                    bInvalidUserInput = true;
+                    bGoing = false;
+                    lastWarning = "Podano zbyt duże n!";
+                    return 0;
+                }
+            }
+            catch(System.FormatException)
+            {
+                bInvalidUserInput = true;
+                bGoing = false;
+                lastWarning = "Nie podano n!";
+                return 0;
+            }
+            bInvalidUserInput = false;
+            lastWarning = "";
+            return Convert.ToByte(text);
+        }
+
+        private void ShowWarning()
+        {
+            warningLabel.Text = lastWarning;
+            warningLabel.Visible = true;
+        }
+        
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (!bGoing)
+            {
+                progressLabel.Visible = false;
+                richTextBox1.SelectAll();
+                richTextBox1.Text = "";
+
+                bGoing = true;
+                byte n = GetNbElements();
+                if (n > 10)
+                {
+                    bPrintOutput = false;
+                    richTextBox1.Text = "Wyświetlanie permutacji jest pomijane ze względu na ich liczbę.";
+                }
+                else bPrintOutput = true;
+                if (bInvalidUserInput)
+                {
+                    ShowWarning();
+                    bGoing = false;
+                    return;
+                }
+                warningLabel.Visible = false;
+                monitorThread = new Thread(UpdateProgressInfos);
+                computationThread = new Thread(ComputePerms);
+                monitorThread.Start(n);
+                computationThread.Start(n);
+            }
+        }
+        private static int Factorial(int n)
+        {
+            if (n == 0) return 1;
+            int result = 1;
+            for (int i = 1; i <= n; i++) result *= i;
+            return result;
+        }
+
+        private void ComputePerms(object elementsCount)
+        {
+            byte n = (byte)elementsCount;
+            InitProgressBar(Factorial(n));
+            GeneratePerms(n);
+            computationThread.Join();
+        }
+
+        private void InitProgressBar(int max)
+        {
+            if(progressBar1.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(InitProgressBar);
+                progressBar1.Invoke(d, new object[] { max });
+            }
+            else 
+            {
+                progressBar1.Visible = true;
+                progressBar1.Value = 1;
+                progressBar1.Minimum = 1;
+                progressBar1.Maximum = max;
+                progressBar1.Step = 1;
+            }
+        }
+
+        private void UpdateProgressInfos(object elementsCount)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            TimeSpan elapsed = new TimeSpan();
+            byte n = (byte)elementsCount;
+            int NbPerms = Factorial(n);
+            SetNbPerms(NbPerms);
+            int currNbPerms = lp + 1;
+            while (bGoing)
+            {
+                stopwatch.Stop();
+                elapsed += stopwatch.Elapsed;
+                stopwatch.Restart();
+                int timeLeft;
+                if(bPrintOutput)
+                { 
+                    timeLeft = (int)CalcRemainingTime(currNbPerms, NbPerms, elapsed);
+                    UpdateRemainingTime(timeLeft);
+                }
+                else UpdateProgress(lp, NbPerms);
+                Thread.Sleep(refreshTime);
+            }
+            UpdateRemainingTime(-1);
+            stopwatch.Stop();
+            monitorThread.Join();
+        }
+        private void UpdateProgress(int curr, int NbPerms)
+        {
+            if (progressLabel.InvokeRequired)
+            {
+                var d = new SafeCallDelegate4(UpdateProgress);
+                progressLabel.Invoke(d, new object[] { curr, NbPerms });
+            }
+            else
+            {
+                progressLabel.Visible = true;
+                float truncated = (float)(Math.Truncate((double)curr / NbPerms * 1000.0) / 1000.0);
+                progressLabel.Text = truncated.ToString() + "%";
+            }
+        }
+        private double CalcRemainingTime( int currPerms, int totalPerms, TimeSpan currTime) //in seconds
+        {
+            double seconds = currTime.Days * 86400 + currTime.Hours * 3600 + currTime.Minutes * 60 + currTime.Seconds + (double)currTime.Milliseconds / 1000;
+            double speed = currPerms / seconds;
+            double remainingTime = (totalPerms - currPerms) / speed;
+            return Math.Round(remainingTime);
+        }
+
+        private void UpdateRemainingTime(int timeLeft)
+        { 
+            if(progressLabel.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(UpdateRemainingTime);
+                try {progressLabel.Invoke(d, new object[] { timeLeft }); }
+                catch(System.ComponentModel.InvalidAsynchronousStateException) { computationThread.Join(); }
+            }
+            else
+            {
+                progressLabel.Visible = true;
+                if (timeLeft > 59) progressLabel.Text = "Pozostało: " + (timeLeft / 60).ToString() + "min " + (timeLeft % 60).ToString() + "s";
+                else if (timeLeft > 4) progressLabel.Text = "Pozostało: " + timeLeft.ToString() + "s";
+                else if (timeLeft > 1) progressLabel.Text = "Pozostały: " + timeLeft.ToString() + "s";
+                else if (timeLeft == 1) progressLabel.Text = "Pozostała: 1s";
+                else if (timeLeft == -1) progressLabel.Text = "Ukończono";
+                else progressLabel.Text = "Kończenie...";
+            }
+        }
+
+        private void SetNbPerms(int nb)
+        {
+            if(textBox1.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(SetNbPerms);
+                textBox1.Invoke(d, new object[] { nb });
+            }
+            else textBox1.Text = nb.ToString();
+
+        }
+        //sequential Heap's algorithm
+        private void GeneratePerms(int n)
+        {
+            List<byte> pseudoStack = new List<byte>();
+            for (byte i = 0; i < n; i++) { pseudoStack.Add(0); }
+            List<byte> permSet = new List<byte>();
+            for (byte i = 0; i < n; i++) { permSet.Add(i); }
+            if (bPrintOutput)
+            {
+                PrintPerm(permSet, lp);
+                lp++;
+            }
+            byte stackIndex = 0;
+            bool bPermFound;
+            while(stackIndex < n)
+            {
+                bPermFound = false;
+                if(pseudoStack[stackIndex] < stackIndex)
+                {
+                    if (stackIndex % 2 == 0) Swap(permSet, 0, stackIndex);
+                    else Swap(permSet, pseudoStack[stackIndex], stackIndex);
+                    if (bPrintOutput){ PrintPerm(permSet, lp);}
+                    lp++;
+                    bPermFound = true;
+                    pseudoStack[stackIndex]++;
+                    stackIndex = 0;
+                }
+                else
+                {
+                    pseudoStack[stackIndex]=0;
+                    stackIndex++;
+                }
+                if (bPermFound) UpdateProgressBar(); 
+            }
+            bGoing = false;
+            lp = 0;
+        }
+
+        private void UpdateProgressBar()
+        {
+            if(progressBar1.InvokeRequired)
+            {
+                var d = new SafeCallDelegate3(UpdateProgressBar);
+                try { progressBar1.Invoke(d); }
+                catch (System.ComponentModel.InvalidAsynchronousStateException) { computationThread.Join(); }
+            }
+            else progressBar1.PerformStep();
+        }
+
+        public void Swap(List<byte>list, byte indexA, byte indexB)
+        {
+            byte tmp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = tmp;
+        }
+
+        private void PrintPerm(List<byte> permSet, int lp)
+        {
+            string perm = lp.ToString() + ".\t[ ";
+            foreach (byte element in permSet) { perm += element.ToString() + " "; }
+            perm += "]\n";
+            if (richTextBox1.InvokeRequired)
+            {
+                var d = new SafeCallDelegate2(PrintPerm);
+                try { richTextBox1.Invoke(d, new object[] { permSet, lp }); }
+                catch (System.ComponentModel.InvalidAsynchronousStateException) { computationThread.Join(); }
+            }
+            else richTextBox1.AppendText(perm);
+        }
+    }
+}
